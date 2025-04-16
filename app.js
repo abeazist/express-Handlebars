d27edc541784efacb23e39dff6bd1f41d077fe97
abeazist <128046453+abeazist@users.noreply.github.com>
2025-04-16 10:59:43 -0300

    feito

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 8086;
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+//configuração do handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+//config do body-parser
+app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.json());
+
+let arr_postagens = [];
+
+
+app.get('/', function(req, res){
+    res.end("oie nanda");
+});
+
+app.get('/formulario', function(req, res){
+    res.render('formulario');
+});
+
+app.post('/cadastrarPostagem', function(req, res){
+    //console.log(req.body.titulo)
+    //console.log(req.body.conteudo)
+    //res.send("postagem recebida!")
+    arr_postagens.push({titulo: req.body.titulo,  conteudo: req.body.conteudo});
+    res.render('postagens', {arr_postagens})
+})
+
+app.get('/postagens', function(req, res){
+    res.render('postagens', {arr_postagens})
+})
+
+app.listen(port, function(req, res){
+    console.log(`Rodaaaaando ${port}`)
+});
